ed7259ee8a7b76f70ae122d2e92c021f49804ced
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2024-06-04 18:58:29 +0100

    Set up useState for images

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React, { useState } from 'react';
+import Slider from 'react-slick';
 import './index.css';
+import image1 from '../public/assets/Beauty Tuesday.jpg';
+import image2 from '../public/assets/Brita Water.jpg';
+import image3 from '../public/assets/Exclusive Products On Jumia.jpg';
+import image4 from '../public/assets/Flash Sales.jpg';
+import image5 from '../public/assets/Jumia Shopping Festival.jpg';
+import image6 from '../public/assets/JumiaPay Providus Bank.jpg';
+import image7 from '../public/assets/MayBelline MakeUp Products.jpg';
+
 
 const App = () => {
-  const [slider, setNewSlider] = useState(" ");
+    const [images, setImages] = useState([
+      'image1.jpg',
+      'image2.jpg',
+      'image3.jpg',
+      'image4.jpg',
+      'image5.jpg',
+      'image6.jpg',
+      'image7.jpg',
+    ]);
 
-  const handleSwipe = () => {
-    setNewSlider(" "); 
-  };
+  
 
   return (
     <div className="parent-container">
